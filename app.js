f329b20ae1ae8bf8dfc264361365aebf62afb972
LeninYasselTrinidadF <66233437+LeninYasselTrinidadF@users.noreply.github.com>
2025-11-19 03:04:02 -0500

    Add functions to guardar and leer apuntes

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// GUARDAR UN APUNTE
+export async function guardarAunte(titulo, contenido) {
+  await addDoc(collection(db, "apuntes"), {
+    titulo,
+    contenido,
+    area,
+    nivel,
+    fecha: Date.now()
+  });
+}
+
+// LEER APUNTES
+export async function leerApuntes() {
+  const snap = await getDocs(collection(db, "apuntes"));
+  const lista = [];
+  snap.forEach(doc => lista.push({ id: doc.id, ...doc.data() }));
+  return lista;
+}
 
